dd3d2bb48d456a52a65533cf18a96029f8e0927f
Paul Done <pkdone@gmail.com>
2020-09-13 15:24:42 +0100

    Split out stage module

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use tokio::{
 };
 use regex::Regex;
 use trust_dns_resolver::{
-     AsyncResolver, TokioAsyncResolver, TokioConnection, TokioConnectionProvider,
+    AsyncResolver, TokioAsyncResolver, TokioConnection, TokioConnectionProvider,
     error::ResolveError,
 };
 use clap::{App, Arg};
@@ -25,83 +25,12 @@ use mongodb::{
     error::{Error as MongoError, ErrorKind as MongoErrorKind},
 };
 
+mod stage;
+pub use crate::{
+    stage::StageState, stage::StageStatus, stage::STAGE1, stage::STAGE2, stage::STAGE3,
+    stage::STAGE4, stage::STAGE5, stage::STAGE6, stage::STAGE7, stage::STAGES,
+};
 
-const STAGE0: usize = 0;
-const STAGE1: usize = 1;
-const STAGE2: usize = 2;
-const STAGE3: usize = 3;
-const STAGE4: usize = 4;
-const STAGE5: usize = 5;
-const STAGE6: usize = 6;
-const STAGE7: usize = 7;
-const TOTAL_STAGES: usize = STAGE7 + 1;
-
-
-struct Stage {
-    index: usize,
-    name: &'static str,
-    desc: &'static str,
-}
-
-
-const STAGES: [Stage; TOTAL_STAGES] = [
-    Stage {
-        index: STAGE0,
-        name: "<setup>",
-        desc: "<setup>",
-    },
-    Stage {
-        index: STAGE1,
-        name: "URL-CHECK",
-        desc: "Confirm URL contains seed list of target server(s) or service name",
-    },
-    Stage {
-        index: STAGE2,
-        name: "MEMBERS-CHECK",
-        desc: "Determine list of individual servers (look up DNS SRV service if defined)",
-    },
-    Stage {
-        index: STAGE3,
-        name: "DNS-IP-CHECK",
-        desc: "Determine the IP addresses of each individual server, via DNS",
-    },
-    Stage {
-        index: STAGE4,
-        name: "SOCKET-CHECK",
-        desc: "Confirm TCP socket can be established to one or more target servers",
-    },
-    Stage {
-        index: STAGE5,
-        name: "DRIVER-CHECK",
-        desc: "Confirm driver can validate the URL (including SRV resolution if required)",
-    },
-    Stage {
-        index: STAGE6,
-        name: "DBPING-CHECK",
-        desc: "Confirm driver can connect to deployment using 'dbping'",
-    },
-    Stage {
-        index: STAGE7,
-        name: "HEALTH-CHECK",
-        desc: "Retrieve running deployment's member composition including which is primary",
-    },
-];
-
-
-#[derive(Debug)]
-enum StageState {
-    NotApplicable,
-    NotTested,
-    Passed,
-    Failed,
-}
-
-
-struct StageStatus {
-    index: usize,
-    state: StageState,
-    advice: Vec<String>,
-}
 
 struct HostnameIP4AddressMap {
     hostname: String,
@@ -191,7 +120,7 @@ fn main() {
 //
 fn start(url: &str, username: Option<&str>, password: Option<&str>) {
     print_intro_with_stages();
-    let mut stages_status = initialise_stages_status();
+    let mut stages_status = StageStatus::new();
     println!("Specified deployment URL:");
     println!("  '{}'", url);
     println!();
@@ -491,8 +420,8 @@ async fn stage4_ip_socket_check(stage_index: usize, stages_status : &mut [StageS
 
 // Confirm driver can validate the URL (including SRV resolution if required
 //
-async fn stage5_driver_check(stage_index: usize, stages_status : &mut [StageStatus], url: &str,
-                             usr: Option<&str>, pwd: Option<&str>)
+async fn stage5_driver_check(stage_index: usize, stages_status : &mut [StageStatus],
+                             url: &str, usr: Option<&str>, pwd: Option<&str>)
                              -> Result<ClientOptions, MongoError> {
     print_stage_header(stage_index);
     stages_status[stage_index].state = StageState::Failed;
@@ -1070,55 +999,6 @@ fn print_stage_header(stage_index: usize) {
     println!();
     println!("----- STAGE {:#?} ({}) -----", stage_index, STAGES[stage_index].name);   
     println!();
-}
-
-
-// Initialise the stages check tracking structure to all be 'NotTested', initially
-// 
-fn initialise_stages_status()
-                            -> [StageStatus; TOTAL_STAGES] {
-    [
-        StageStatus {
-            index: STAGE0,
-            state: StageState::NotApplicable,
-            advice: vec![],
-        },
-        StageStatus {
-            index: STAGE1,
-            state: StageState::NotTested,
-            advice: vec![],
-        },
-        StageStatus {
-            index: STAGE2,
-            state: StageState::NotTested,
-            advice: vec![],
-        },
-        StageStatus {
-            index: STAGE3,
-            state: StageState::NotTested,
-            advice: vec![],
-        },
-        StageStatus {
-            index: STAGE4,
-            state: StageState::NotTested,
-            advice: vec![],
-        },
-        StageStatus {
-            index: STAGE5,
-            state: StageState::NotTested,
-            advice: vec![],
-        },
-        StageStatus {
-            index: STAGE6,
-            state: StageState::NotTested,
-            advice: vec![],
-        },
-        StageStatus {
-            index: STAGE7, 
-            state: StageState::NotTested,
-            advice: vec![],
-        },
-    ]
 }
 
 
---
+++ src/stage/mod.rs
@@ -0,0 +1,131 @@
+use std::str;
+
+
+const STAGE0: usize = 0;
+pub const STAGE1: usize = 1;
+pub const STAGE2: usize = 2;
+pub const STAGE3: usize = 3;
+pub const STAGE4: usize = 4;
+pub const STAGE5: usize = 5;
+pub const STAGE6: usize = 6;
+pub const STAGE7: usize = 7;
+const TOTAL_STAGES: usize = STAGE7 + 1;
+
+
+pub struct Stage {
+    pub index: usize,
+    pub name: &'static str,
+    pub desc: &'static str,
+}
+
+
+pub const STAGES: [Stage; TOTAL_STAGES] = [
+    Stage {
+        index: STAGE0,
+        name: "<setup>",
+        desc: "<setup>",
+    },
+    Stage {
+        index: STAGE1,
+        name: "URL-CHECK",
+        desc: "Confirm URL contains seed list of target server(s) or service name",
+    },
+    Stage {
+        index: STAGE2,
+        name: "MEMBERS-CHECK",
+        desc: "Determine list of individual servers (look up DNS SRV service if defined)",
+    },
+    Stage {
+        index: STAGE3,
+        name: "DNS-IP-CHECK",
+        desc: "Determine the IP addresses of each individual server, via DNS",
+    },
+    Stage {
+        index: STAGE4,
+        name: "SOCKET-CHECK",
+        desc: "Confirm TCP socket can be established to one or more target servers",
+    },
+    Stage {
+        index: STAGE5,
+        name: "DRIVER-CHECK",
+        desc: "Confirm driver can validate the URL (including SRV resolution if required)",
+    },
+    Stage {
+        index: STAGE6,
+        name: "DBPING-CHECK",
+        desc: "Confirm driver can connect to deployment using 'dbping'",
+    },
+    Stage {
+        index: STAGE7,
+        name: "HEALTH-CHECK",
+        desc: "Retrieve running deployment's member composition including which is primary",
+    },
+];
+
+
+#[derive(Debug)]
+pub enum StageState {
+    NotApplicable,
+    NotTested,
+    Passed,
+    Failed,
+}
+
+
+pub struct StageStatus {
+    pub index: usize,
+    pub state: StageState,
+    pub advice: Vec<String>,
+}
+
+
+// Initialise the stages check tracking structure to all be 'NotTested', initially
+// 
+impl StageStatus {
+    pub fn new()
+           -> [StageStatus; TOTAL_STAGES] {
+        [
+            StageStatus {
+                index: STAGE0,
+                state: StageState::NotApplicable,
+                advice: vec![],
+            },
+            StageStatus {
+                index: STAGE1,
+                state: StageState::NotTested,
+                advice: vec![],
+            },
+            StageStatus {
+                index: STAGE2,
+                state: StageState::NotTested,
+                advice: vec![],
+            },
+            StageStatus {
+                index: STAGE3,
+                state: StageState::NotTested,
+                advice: vec![],
+            },
+            StageStatus {
+                index: STAGE4,
+                state: StageState::NotTested,
+                advice: vec![],
+            },
+            StageStatus {
+                index: STAGE5,
+                state: StageState::NotTested,
+                advice: vec![],
+            },
+            StageStatus {
+                index: STAGE6,
+                state: StageState::NotTested,
+                advice: vec![],
+            },
+            StageStatus {
+                index: STAGE7,
+                state: StageState::NotTested,
+                advice: vec![],
+            },
+        ]
+    }
+}   
+
